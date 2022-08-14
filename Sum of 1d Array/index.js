6e69a288f62d68c64df393f5066b5a39034921ce
rin4ik <m.mirakhmad@gmail.com>
2022-08-14 18:38:42 +0500

    minor improve

--- Sum of 1d Array/index.js
+++ Sum of 1d Array/index.js
@@ -3,12 +3,10 @@
  * @return {number[]}
  */
  var runningSum = function(nums) {
-    let transformed = [];
     for(let i = 0; i < nums.length; i++) {
     	const val = nums[i];
-        const oldVal = transformed[i - 1] || 0
-    	transformed.push(oldVal+val)
+        const oldVal = nums[i - 1] || 0
+    	nums[i] = oldVal+val
     }
-    return transformed
+    return nums;
 };
-
