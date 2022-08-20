a87702a6ff8925d573a89595dd0001850a0eb201
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2022-08-20 12:48:06 +0500

    polindrome

---
+++ Polindrome/README.md
@@ -0,0 +1,16 @@
+
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that’s spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note. You’ll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
+
+We’ll pass strings with varying formats, such as <b>“racecar”</b>, <b>“RaceCar”</b>, and <b>“race CAR”</b> among others.
+<pre>
+<code>
+function palindrome(str) {
+  return true;
+}
+palindrome("eye");
+</pre>
+</code>
---
+++ Polindrome/index.js
@@ -0,0 +1,22 @@
+ // first way using str split and reverse array and checking if they are equal O(n)
+function palindrome(str) {
+	str = str.replace(/[^\w]/gi, '').toLowerCase() 
+    return str === str.split('').reverse().join('')
+}
+ // # second way is more better using Pointer method
+ // O(n/2) will will loop twice faster using left and right pointer
+
+function palindrome(str) {
+	str = str.replace(/[^\w]/gi, '').toLowerCase()
+    let left = 0
+    let right = str.length - 1
+    while(left < right) {
+        if(str[left] !== str[right]) return false
+        left++
+        right--
+    }
+    return true
+}
+
+
+console.log(palindrome('radar'))
